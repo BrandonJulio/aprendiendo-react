08eef3d5a7835b619d7ef954077d32d459a80ffd
BrandonJulio <bjulio@unicesar.edu.co>
2024-11-01 01:08:14 -0500

    Extract file save to storage, juego teminado

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -6,12 +6,21 @@ import { Square } from "./components/Square.jsx";
 import { TURNS} from "./constants.js";
 import { checkWinnerFrom, checkEndGame } from "./logic/board.js";
 import { WinnerModal } from "./components/WinnerModal.jsx";
-
+import { resetGameStorage, saveGameToStorage } from "./logic/storage/index.js";
 
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() =>{
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+  
+  const [turn, setTurn] = useState(()=>{
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
   const [winner, setWinner] = useState(null) //null es que no hay ganador, false es que
   // hay un empate
 
@@ -19,6 +28,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   const updateBoard = (index)=>{ 
@@ -32,6 +43,11 @@ function App() {
     // Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // Guardar partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
     // Verificar si hay un ganador
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
